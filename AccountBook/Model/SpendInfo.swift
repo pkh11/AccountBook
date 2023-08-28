7b1a83cc4b694bd63ecd6c51c00a69a416d5c52b
pkh <toby369369@naver.com>
2023-08-28 20:37:31 +0900

    :construction: Realm 모델 생성

--- AccountBook/Model/SpendInfo.swift
+++ AccountBook/Model/SpendInfo.swift
@@ -10,6 +10,7 @@ import Foundation
 import RealmSwift
 
 internal final class SpendInfo: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var amount: Float
     @Persisted var date: Date
     @Persisted var type: String
@@ -17,5 +18,13 @@ internal final class SpendInfo: Object {
     
     var amountFloatToInt: Int {
         return Int(amount)
+    }
+    
+    convenience init(_ amount: Float, _ date: Date, _ type: String, _ text: String) {
+        self.init()
+        self.amount = amount
+        self.date = date
+        self.type = type
+        self.text = text
     }
 }
